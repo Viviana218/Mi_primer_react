d3f2f8f7c12faa896d0d8b0978f42147e4b058e0
Viviana218 <104119912+Viviana218@users.noreply.github.com>
2022-09-27 20:49:07 -0500

    inicializando

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component {
+  render () {
+    return(
+      <div className="container">
+        <h1> Hello Dojo!</h1>
+        <h1> Things I need to do: </h1>
+        <ul>
+          <li>Learn React</li>
+          <li>Learn Python</li>
+          <li>Learn Flask</li>
+          <li>Learn Jinja</li>
+          <li>Learn Node</li>
+          <li>Learn Npm</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
